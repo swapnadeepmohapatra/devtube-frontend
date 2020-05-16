0921c65b8b425db8e4efc688f460e108d2ee6a04
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-05-16 23:04:52 +0530

    added isAuthenticated method

--- src/helper/authCalls.js
+++ src/helper/authCalls.js
@@ -40,3 +40,13 @@ export const authenticate = (data, next) => {
 		next();
 	}
 };
+
+export const isAuthenticated = () => {
+	if (typeof window == 'undefined') {
+		return false;
+	} else if (localStorage.getItem('jwt')) {
+		return JSON.parse(localStorage.getItem('jwt'));
+	} else {
+		return false;
+	}
+};
