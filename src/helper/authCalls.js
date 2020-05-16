aced9f163c492a4c28a555059bd6fbff9a2dae15
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-05-16 23:03:02 +0530

    added signin call

--- src/helper/authCalls.js
+++ src/helper/authCalls.js
@@ -16,3 +16,20 @@ export const signup = (user) => {
 			return console.log(error);
 		});
 };
+
+export const signin = (user) => {
+	return fetch(`${API}signin`, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/JSON',
+			'Content-Type': 'application/JSON',
+		},
+		body: JSON.stringify(user),
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.catch((error) => {
+			return console.log(error);
+		});
+};
