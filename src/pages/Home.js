f023209270e16efb554c75ad666935621313d979
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-05-18 23:15:36 +0530

    added videos on to the home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../helper/api';
+import HoverVideoPlayer from 'react-hover-video-player';
+import { CircularProgress } from '@material-ui/core';
+import '../css/home.css';
 
 function Home() {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		api.get('getVideos').then((resp) => {
+			console.log(resp.data.videos);
+			setVideos(resp.data.videos);
+		});
+	}, []);
+
 	return (
-		<div className="app">
-			<h1>Welcome to Home Page</h1>
+		<div>
+			<h1>Recommended</h1>
+			<ul className="videos-ul">
+				{videos &&
+					videos.map((video) => {
+						// return <div>{JSON.stringify(video)}</div>;
+						return (
+							<li className="list-item" key={video._id}>
+								<div className="video-list-item">
+									<HoverVideoPlayer
+										videoSrc={video.filePath}
+										pausedOverlay={
+											<img src={video.thumbnail} alt="" className="video-list-thumb" />
+										}
+										loadingOverlay={<CircularProgress />}
+									/>
+								</div>
+								<div className="video-title">
+									<img src={video.writer.image} alt="" className="account-img" />
+									<h3>{video.title}</h3>
+								</div>
+								<h4>{video.writer.name}</h4>
+							</li>
+						);
+					})}
+			</ul>
 		</div>
 	);
 }
